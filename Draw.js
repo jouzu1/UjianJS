5a8ed2792a3a9761ed52cab9c88c92f787548e49
jouzu1 <jouzu1@gmail.com>
2021-04-23 11:52:07 +0700

    update jawaban terakhir

--- Draw.js
+++ Draw.js
@@ -39,6 +39,30 @@ class Draw{
         }
         console.log("\n")
     }
+
+    gambarKotakPola(p){
+        for(let i = 0 ; i<p ; i++){
+            for(let j = 0 ; j<p ; j++){
+                if(i==0 && j==2){
+                    process.stdout.write("!")
+                }else if(i==1 && j==1){
+                    process.stdout.write("!")
+                }else if(i==2 && j==2){
+                    process.stdout.write("!")
+                }
+                else if(i==3 && j==1){
+                    process.stdout.write("!")
+                }else if(i==4 && j==2){
+                    process.stdout.write("!")
+                }
+                else{
+                    process.stdout.write("*")
+                }
+                
+            }console.log(" ")
+        }
+        console.log("\n")
+    }
 }
 
 let kotak = new Draw();
@@ -48,7 +72,10 @@ let segitiga = new Draw();
 segitiga.gambarSegitiga(5)
 
 let segitigaTerbalik = new Draw();
-segitigaTerbalik.gambarSegitigaTerbalik(6);
+segitigaTerbalik.gambarSegitigaTerbalik(5);
 
 let selangseling = new Draw();
 selangseling.gambarSelangSeling(5);
+
+let kotakpola = new Draw();
+kotakpola.gambarKotakPola(5);
